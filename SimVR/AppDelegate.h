c41d898b53756747e0a773ad36bc64221949a960
jessiedeot <jessiedeot@gmail.com>
2016-04-16 20:56:22 -0700

    Initial Commit

---
+++ SimVR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimVR
+//
+//  Created by Macintosh User on 4/16/16.
+//  Copyright © 2016 JessieDeot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimVR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimVR
+//
+//  Created by Macintosh User on 4/16/16.
+//  Copyright © 2016 JessieDeot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
